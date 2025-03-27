65ca3e2d0bb29879025f47ace55161c7e3be837b
EldarToleubay <49515746+EldarToleubay@users.noreply.github.com>
2025-03-27 17:12:31 +0500

    add: log service

--- src/main/java/com/alibou/security/auth/AuthenticationController.java
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,28 +17,37 @@ import java.io.IOException;
 @CrossOrigin(origins = "https://skffront.netlify.app")
 public class AuthenticationController {
 
-  private final AuthenticationService service;
+    private final AuthenticationService service;
 
-  @PostMapping("/register")
-  public ResponseEntity<AuthenticationResponse> register(
-      @RequestBody RegisterRequest request
-  ) {
-    return ResponseEntity.ok(service.register(request));
-  }
-  @PostMapping("/authenticate")
-  public ResponseEntity<AuthenticationResponse> authenticate(
-      @RequestBody AuthenticationRequest request
-  ) {
-    return ResponseEntity.ok(service.authenticate(request));
-  }
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
 
-  @PostMapping("/refresh-token")
-  public void refreshToken(
-      HttpServletRequest request,
-      HttpServletResponse response
-  ) throws IOException {
-    service.refreshToken(request, response);
-  }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
+    }
+
+
+    @GetMapping("/logs")
+    public ResponseEntity<String> getLogs() throws IOException {
+        Path logFile = Paths.get(System.getProperty("logging.file.name"));
+        String logs = new String(Files.readAllBytes(logFile));
+        return ResponseEntity.ok(logs);
+    }
 
 
 }
