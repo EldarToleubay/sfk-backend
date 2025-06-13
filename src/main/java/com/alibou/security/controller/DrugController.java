7853f58ef3607d2c972f57e8b228b29f33d0c745
JusanUser <eldartoleubay@gmail.com>
2025-06-13 21:19:55 +0500

    Revert "add: new drug reg logic"
    This reverts commit e73e4716782f39000237151fa25cad2153c00eb4.

--- src/main/java/com/alibou/security/controller/DrugController.java
+++ src/main/java/com/alibou/security/controller/DrugController.java
@@ -2,7 +2,6 @@ package com.alibou.security.controller;
 
 import com.alibou.security.entity.Drug;
 import com.alibou.security.service.DrugService;
-import com.alibou.security.service.ReferenceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,11 +13,9 @@ import java.util.List;
 public class DrugController {
 
     private final DrugService drugService;
-    private final ReferenceService referenceService;
 
-    public DrugController(DrugService drugService, ReferenceService referenceService) {
+    public DrugController(DrugService drugService) {
         this.drugService = drugService;
-        this.referenceService = referenceService;
     }
 
     @GetMapping
@@ -31,12 +28,10 @@ public class DrugController {
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
             drugService.importExcel(file);
-            referenceService.setAll();
             return ResponseEntity.ok("Файл успешно обработан и данные сохранены");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Ошибка при обработке файла: " + e.getMessage());
         }
     }
-
 }
 
