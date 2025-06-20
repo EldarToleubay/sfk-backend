b222d4e872daad5321529d9c6ab251f815330281
JusanUser <eldartoleubay@gmail.com>
2025-06-20 20:58:54 +0500

    add: cross origin access

--- src/main/java/com/alibou/security/useraccess/UserAccessController.java
+++ src/main/java/com/alibou/security/useraccess/UserAccessController.java
@@ -4,10 +4,7 @@ import com.alibou.security.user.User;
 import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +14,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/access")
+@CrossOrigin(origins = "*")
 public class UserAccessController {
 
     private final UserRepository userRepository;
