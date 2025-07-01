4eb344eddf7740a7446536389333d2335126ae9e
EldarToleubay <49515746+EldarToleubay@users.noreply.github.com>
2025-07-01 21:14:52 +0500

    fix: default response to /top requests without filter parameters

--- src/main/java/com/alibou/security/config/SecurityConfiguration.java
+++ src/main/java/com/alibou/security/config/SecurityConfiguration.java
@@ -37,6 +37,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration {
 
     private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/api/bexsex",
             "/v2/api-docs",
             "/v2/api-docs",
             "/v3/api-docs",
